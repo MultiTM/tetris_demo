0ff08658bab74b66bb595604725671c5a0cc7073
Shablodor <ilyxa315@gmail.com>
2023-12-22 11:13:43 +0300

    Refactor

--- Assets/_Project/_Scripts/Infrastructure/Installers/GameplayInstaller.cs
+++ Assets/_Project/_Scripts/Infrastructure/Installers/GameplayInstaller.cs
@@ -16,17 +16,37 @@ namespace _Project._Scripts.Infrastructure
         
         public override void InstallBindings()
         {
+            InstallCore();
+            InstallInstances();
+            InstallConfigs();
+            InstallFactories();
+        }
+
+        private void InstallCore()
+        {
             Container.Bind<Field>().AsSingle();
             Container.Bind<PieceGenerator>().AsSingle();
             Container.Bind<PieceQueue>().AsSingle();
+        }
+
+        private void InstallInstances()
+        {
             Container.Bind<NextPieceRenderer>().FromInstance(_nextPieceRenderer).AsSingle();
             Container.Bind<InputHandler>().FromInstance(_inputHandler).AsSingle();
             Container.Bind<FieldRenderer>().FromInstance(_fieldRenderer).AsSingle();
             Container.Bind<FieldTicker>().FromInstance(_fieldTicker).AsSingle();
-            Container.Bind<FieldSettings>().FromScriptableObject(_fieldSettings).AsSingle();
-            Container.Bind<TetraminoConfig>().FromScriptableObject(_tetraminoConfig).AsSingle();
+        }
+
+        private void InstallFactories()
+        {
             Container.BindFactory<FieldCell, FieldCell.Factory>().FromComponentInNewPrefab(_fieldCell);
             Container.BindFactory<Vector2Int[], Tetramino, Tetramino.Factory>();
+        }
+
+        private void InstallConfigs()
+        {
+            Container.Bind<FieldSettings>().FromScriptableObject(_fieldSettings).AsSingle();
+            Container.Bind<TetraminoConfig>().FromScriptableObject(_tetraminoConfig).AsSingle();
         }
     }
 }
